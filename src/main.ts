0e25c79da18542310fcc6768054d94b9b56a63dd
Nils Ramstöck <nilsramstoeck@gmail.com>
2022-04-01 13:36:46 +0100

    Project setup

---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 8080;
+
+app.get('/', (_req, res) => {
+   res.send('Hello World!');
+});
+
+app.listen(port, () => console.log('server started at ' + port));
---
+++ test/main.test.ts
@@ -0,0 +1,8 @@
+describe('Wormling API', () => {
+
+   test('My first test', () => {
+      const HelloWorld = true;
+      expect(HelloWorld).toBe(true);
+   });
+
+});
